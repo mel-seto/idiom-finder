5c74389c03e7cb17f4899d1183f33be4ec4afe02
Mel Seto <mel-seto@users.noreply.github.com>
2025-09-25 10:50:21 -0400

    organizing files into src folder etc.

---
+++ src/utils/utils.py
@@ -0,0 +1,7 @@
+from pypinyin import Style, pinyin
+
+
+def get_pinyin(text: str):
+    """Convert Chinese characters to pinyin with tones."""
+    py_list = pinyin(text, style=Style.TONE, heteronym=False)
+    return " ".join([syllable[0] for syllable in py_list])
