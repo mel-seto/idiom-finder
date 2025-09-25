46281de8294ecc473ff9a2b56d3c96a374832dd5
Mel Seto <mel-seto@users.noreply.github.com>
2025-09-25 10:50:21 -0400

    organizing files into src folder etc.

---
+++ src/run.py
@@ -0,0 +1,14 @@
+from multiprocessing import freeze_support
+
+from watchfiles import run_process
+
+import app  # Import app module
+
+
+def start_app():
+    app.launch_app()
+
+
+if __name__ == "__main__":
+    freeze_support()
+    run_process(".", target=start_app)
